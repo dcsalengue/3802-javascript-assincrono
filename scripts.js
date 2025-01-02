f6e28b301ad942aacb23f309bfb833aef256287e
dcsalengue <dcsalengue@me.com>
2025-01-01 23:28:26 -0300

    Aula 5

--- scripts.js
+++ scripts.js
@@ -81,23 +81,6 @@ inputTags.addEventListener("keypress", async (evento) => {
     }
 });
 
-const botaoPublicar = document.querySelector(".botao-publicar");
-
-botaoPublicar.addEventListener("click", async (evento) => {
-    evento.preventDefault();
-    const nomeDoProjeto = document.getElementById("nome").value;
-    const descricaoDoProjeto = document.getElementById("descricao").value;
-    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
-
-    try {
-        const mensagem = await publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto);
-        console.log(mensagem);
-        alert(mensagem);
-    } catch (error) {
-        console.error(error);
-        alert(error);
-    }
-});
 
 async function publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto) {
     return new Promise((resolve, reject) => {
@@ -112,4 +95,34 @@ async function publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto) {
     })
 }
 
+const botaoPublicar = document.querySelector(".botao-publicar");
 
+botaoPublicar.addEventListener("click", async (evento) => {
+    evento.preventDefault();
+    const nomeDoProjeto = document.getElementById("nome").value;
+    const descricaoDoProjeto = document.getElementById("descricao").value;
+    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
+
+    try {
+        const resultado = await publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto);
+        console.log(resultado);
+        alert("Deu tudo certo!")
+    } catch (error) {
+        console.log("Deu errado: ", error)
+        alert("Deu tudo errado!");
+    }
+});
+
+const botaoDescartar = document.querySelector(".botao-descartar");
+
+botaoDescartar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+
+    const formulario = document.querySelector("form");
+    formulario.reset();
+
+    imagemPrincipal.src = "./img/imagem1.png";
+    nomeDaImagem.textContent = "image_projeto.png";
+
+    listaTags.innerHTML = "";
+})
